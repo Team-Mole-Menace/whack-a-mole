238a182e9ff43a8edbdc86f52e594f1f36831dbd
Erik Withak <ErikAanestad@gmail.com>
2018-05-21 14:05:00 -0700

    pre branch

--- js/App.js
+++ js/App.js
@@ -14,24 +14,29 @@ class App {
     }
 
     startGame() {
-        this.ScoreTimer = new Countdown(this.duration, () => {
+        this.Countdown = new Countdown(this.duration, () => {
             // Store score in localStorage
             // Redirect to leaderboard.html
         });
-        this.ScoreTimer.render();
+        this.sectionScoreTimer.appendChild(this.Countdown.render());
         // this.Moles = new Moles();
+        console.log('section', this.sectionScoreTimer);
     }
 
     render() {
         const dom = gameAppTemplate.content;
 
-        const sectionScoreTimer = dom.getElementById('section-score-timer');
-        const sectionMoleBoard = dom.getElementById('section-mole-board');
+        this.sectionScoreTimer = dom.getElementById('section-score-timer');
+        // const sectionMoleBoard = dom.getElementById('section-mole-board');
 
         this.startGame();
 
-        dom.appendChild(sectionScoreTimer);
-        dom.appendChild(sectionMoleBoard);
+        console.log('this section', this.sectionScoreTimer);
+
+        dom.appendChild(this.sectionScoreTimer);
+        // dom.appendChild(sectionMoleBoard);
+
+        console.log('dom', dom);
 
         return dom;
     }
