4db784ca920ab7466fb604620de5fbceba5165dc
KLPainter <KLPainter@gmail.com>
2018-05-21 14:08:10 -0700

    getting started

--- js/App.js
+++ js/App.js
@@ -1,20 +1,22 @@
 /* exported App */
 /* globals Countdown */
 
+const root = document.getElementById('root');
 const gameAppTemplate = document.getElementById('game-app-template');
 const duration = 30000;
 const name = 'hello world';
 
 class App {
 
-    constructor(/* name, duration */) {
+    constructor(name) {
         this.name = name;
-        this.duration = duration;
+    //    this.duration =  duration is used only by timer.  Not necessary in app.
         this.score = 0;
     }
 
     startGame() {
         this.ScoreTimer = new Countdown(this.duration, () => {
+            // this function is called when the countdown has expired and game is over
             // Store score in localStorage
             // Redirect to leaderboard.html
         });
@@ -23,16 +25,16 @@ class App {
     }
 
     render() {
+        // create dom for timer/score and board
         const dom = gameAppTemplate.content;
-
         const sectionScoreTimer = dom.getElementById('section-score-timer');
         const sectionMoleBoard = dom.getElementById('section-mole-board');
-
-        this.startGame();
-
         dom.appendChild(sectionScoreTimer);
         dom.appendChild(sectionMoleBoard);
+        // append dom to root
+        root.appendChild(dom);
 
-        return dom;
+        // start game
+     //   this.startGame();
     }
 }
