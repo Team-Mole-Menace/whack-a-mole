91f365c0d21e95d0e3bf94bbd6297d80b730f840
KLPainter <KLPainter@gmail.com>
2018-05-22 11:40:27 -0700

    index.html form changes

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('submit').addEventListener('click', (event) => {
+    event.preventDefault();
+    // get values from form
     let player = document.getElementById('player').value;
+    let qty = document.getElementById('moleQty').value;
+    let difficulty = document.getElementById('difficulty').value;
+    // test player name input
     if(player === '') {
         alert('Please enter a name');
         return;
     }
+    // write values to storage
     localStorage.setItem('name', JSON.stringify(player));
+    localStorage.setItem('moleQty', JSON.stringify (qty));
+    localStorage.setItem('difficulty', JSON.stringify (difficulty));
+    // open the next window
     window.location.replace('game.html');
 });
