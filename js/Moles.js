a34b4d9a40a4e3123ec40cbedfd18d01dca7db0b
Erik Withak <ErikAanestad@gmail.com>
2018-05-21 22:26:49 -0700

    infused the Mole classes into the GameApp and added a little styling

--- js/Moles.js
+++ js/Moles.js
@@ -1,30 +1,19 @@
 /* exported Moles */
 /* globals Mole */
-
-const totalMoles = 5;
-
+const moleTemplate = document.getElementById('mole-template');
 
 class Moles {
 
-    constructor() {
+    constructor(moleIsWhacked) {
+        this.moleIsWhacked = moleIsWhacked;
         this.score = 0;
-    
     }
-  
+
     render() {
-        for(let i = 0; i < totalMoles; i++) {
-            const newMole = new Mole(() => {
-                // this function runs when mole is whacked
-                this.score++;
-            });
-            //const newMole = new Mole(this.score);
-            newMole.render();
-        }
+        const dom = moleTemplate.content.cloneNode(true);
+        this.mole = dom.querySelector('img');
+        const newMole = new Mole(this.mole, this.moleIsWhacked);
+        newMole.render();
+        return dom;
     }
-
-
-
 }
-
-const myMoles = new Moles;
-myMoles.render();
